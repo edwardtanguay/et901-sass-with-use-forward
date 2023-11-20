c1934fbde5161a67593091b1aaa862b014ee77fe
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-20 13:20:02 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './sass/main.scss';
+
+function App() {
+	return (
+		<>
+		<h1>Sass with @use/@forward</h1>
+
+		<p className="error-text spacing">This is an error.</p>
+		<p className="info-text spacing">This is an info.</p>
+		<p className="normal-text spacing">This is a normal text.</p>
+		<p className="faded-text spacing">This is a faded text.</p>
+
+		<p className="ball-001 shadow spacing"></p>
+		<p className="ball-002 shadow spacing"></p>
+		<p className="ball-003 spacing"></p>
+		<p className="ball-004 spacing"></p>
+
+		<p className="box-sales spacing"></p>
+		<p className="box-hr spacing"></p>
+		<p className="box-marketing spacing"></p>
+		<p className="box-ceo spacing"></p>
+		<p className="box-dev spacing"></p>
+
+		<p className="rectangle-purple spacing"></p>
+		<p className="rectangle-orchid spacing"></p>
+		<p className="rectangle-goldenrod spacing"></p>
+		<p className="rectangle-tomato spacing"></p>
+		<p className="rectangle-limegreen spacing"></p>
+		</>
+	)
+}
+
+export default App
